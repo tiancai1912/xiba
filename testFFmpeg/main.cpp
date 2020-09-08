0bdeee8ace08b12c9245d450e4691377a6080ac4
tiancai1912 <tiancai1912@sina.com>
2020-09-08 03:21:27 -0700

    update mp4toannexb

--- testFFmpeg/main.cpp
+++ testFFmpeg/main.cpp
@@ -1,118 +1,120 @@
 #include <QCoreApplication>
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <fcntl.h>
+
 extern "C" {
+
 #include <libavutil/imgutils.h>
 #include <libavutil/samplefmt.h>
 #include <libavutil/timestamp.h>
 #include <libavformat/avformat.h>
+
 }
 
-static AVFormatContext *fmt_ctx = NULL;
-static AVCodecContext *video_dec_ctx = NULL, *audio_dec_ctx;
-static int width, height;
-static enum AVPixelFormat pix_fmt;
-static AVStream *video_stream = NULL, *audio_stream = NULL;
-static const char *src_filename = NULL;
-static const char *video_dst_filename = NULL;
-static const char *audio_dst_filename = NULL;
-static FILE *video_dst_file = NULL;
-static FILE *audio_dst_file = NULL;
+#define ADTS_HEADER_SIZE 7
 
-static uint8_t *video_dst_data[4] = {NULL};
-static int      video_dst_linesize[4];
-static int video_dst_bufsize;
+static const char *TAG = "MP4Parser";
 
-static int video_stream_idx = -1, audio_stream_idx = -1;
-static AVFrame *frame = NULL;
-static AVPacket pkt;
+typedef struct {
+    int write_adts;
+    int object_type;
+    int sample_rate_index;
+    int channel_conf;
+} ADTSContext;
 
-/*static int video_frame_count = 0;
-static int audio_frame_count = 0;*/
 
-static int open_codec_context(int *stream_idx,
-                              AVCodecContext **dec_ctx, AVFormatContext *fmt_ctx, enum AVMediaType type)
-{
-    int ret, stream_index;
-    AVStream *st;
-    AVCodec *dec = NULL;
-    AVDictionary *opts = NULL;
-
-    ret = av_find_best_stream(fmt_ctx, type, -1, -1, NULL, 0);
-    if (ret < 0) {
-        fprintf(stderr, "Could not find %s stream in input file '%s'\n",
-                av_get_media_type_string(type), src_filename);
-        return ret;
-    } else {
-        stream_index = ret;
-        st = fmt_ctx->streams[stream_index];
-
-        /* find decoder for the stream */
-        dec = avcodec_find_decoder(st->codecpar->codec_id);
-        if (!dec) {
-            fprintf(stderr, "Failed to find %s codec\n",
-                    av_get_media_type_string(type));
-            return AVERROR(EINVAL);
-        }
-
-        /* Allocate a codec context for the decoder */
-        *dec_ctx = avcodec_alloc_context3(dec);
-        if (!*dec_ctx) {
-            fprintf(stderr, "Failed to allocate the %s codec context\n",
-                    av_get_media_type_string(type));
-            return AVERROR(ENOMEM);
-        }
-
-        /* Copy codec parameters from input stream to output codec context */
-        if ((ret = avcodec_parameters_to_context(*dec_ctx, st->codecpar)) < 0) {
-            fprintf(stderr, "Failed to copy %s codec parameters to decoder context\n",
-                    av_get_media_type_string(type));
-            return ret;
-        }
-
-        /* Init the decoders */
-        if ((ret = avcodec_open2(*dec_ctx, dec, &opts)) < 0) {
-            fprintf(stderr, "Failed to open %s codec\n",
-                    av_get_media_type_string(type));
-            return ret;
-        }
-        *stream_idx = stream_index;
-    }
-
-    return 0;
+static int aac_decode_extradata(ADTSContext *adts, unsigned char *pbuf, int bufsize) {
+      int aot, aotext, samfreindex;
+      int i, channelconfig;
+      unsigned char *p = pbuf;
+      if (!adts || !pbuf || bufsize < 2) {
+            printf("%s, run aac_decode_extradata failed\n", TAG);
+            return -1;
+      }
+      aot = (p[0]>>3)&0x1f;
+      if (aot == 31) {
+            aotext = (p[0]<<3 | (p[1]>>5)) & 0x3f;
+            aot = 32 + aotext;
+            samfreindex = (p[1]>>1) & 0x0f;
+            if (samfreindex == 0x0f) {
+                  channelconfig = ((p[4]<<3) | (p[5]>>5)) & 0x0f;
+            }
+            else {
+                  channelconfig = ((p[1]<<3)|(p[2]>>5)) & 0x0f;
+            }
+      }
+      else {
+            samfreindex = ((p[0]<<1)|p[1]>>7) & 0x0f;
+            if (samfreindex == 0x0f) {
+                  channelconfig = (p[4]>>3) & 0x0f;
+            }
+            else {
+                  channelconfig = (p[1]>>3) & 0x0f;
+            }
+      }
+#ifdef AOT_PROFILE_CTRL
+      if (aot < 2) aot = 2;
+#endif
+      adts->object_type = aot-1;
+      adts->sample_rate_index = samfreindex;
+      adts->channel_conf = channelconfig;
+      adts->write_adts = 1;
+      return 0;
 }
 
-static int get_format_from_sample_fmt(const char **fmt,
-                                      enum AVSampleFormat sample_fmt)
-{
-    int i;
-    struct sample_fmt_entry {
-        enum AVSampleFormat sample_fmt; const char *fmt_be, *fmt_le;
-    } sample_fmt_entries[] = {
-    { AV_SAMPLE_FMT_U8,  "u8",    "u8"    },
-    { AV_SAMPLE_FMT_S16, "s16be", "s16le" },
-    { AV_SAMPLE_FMT_S32, "s32be", "s32le" },
-    { AV_SAMPLE_FMT_FLT, "f32be", "f32le" },
-    { AV_SAMPLE_FMT_DBL, "f64be", "f64le" },
-};
-    *fmt = NULL;
+static int aac_set_adts_head(ADTSContext *acfg, unsigned char *buf, int size) {
+      unsigned char byte;
+      if (size < ADTS_HEADER_SIZE) {
+          printf("%s, run aac_set_adts_head failed, input size < ADTS_HEADER_SIZE\n", TAG);
+          return -1;
+      }
 
-    for (i = 0; i < FF_ARRAY_ELEMS(sample_fmt_entries); i++) {
-        struct sample_fmt_entry *entry = &sample_fmt_entries[i];
-        if (sample_fmt == entry->sample_fmt) {
-            *fmt = AV_NE(entry->fmt_be, entry->fmt_le);
-            return 0;
-        }
-    }
+      buf[0] = 0xff;
+      buf[1] = 0xf1;
+      byte = 0;
+      byte |= (acfg->object_type & 0x03) << 6;
+      byte |= (acfg->sample_rate_index & 0x0f) << 2;
+      byte |= (acfg->channel_conf & 0x07) >> 2;
+      buf[2] = byte;
+      byte = 0;
+      byte |= (acfg->channel_conf & 0x07) << 6;
+      byte |= (ADTS_HEADER_SIZE + size) >> 11;
+      buf[3] = byte;
+      byte = 0;
+      byte |= (ADTS_HEADER_SIZE + size) >> 3;
+      buf[4] = byte;
+      byte = 0;
+      byte |= ((ADTS_HEADER_SIZE + size) & 0x7) << 5;
+      byte |= (0x7ff >> 6) & 0x1f;
+      buf[5] = byte;
+      byte = 0;
+      byte |= (0x7ff & 0x3f) << 2;
+      buf[6] = byte;
 
-    fprintf(stderr,
-            "sample format %s is not supported as output format\n",
-            av_get_sample_fmt_name(sample_fmt));
-    return -1;
+      return 0;
 }
 
 int main(int argc, char *argv[])
 {
+    AVPacket pkt;
+    AVFormatContext *fmt_ctx = NULL;
+
     int ret = 0;
+    int video_stream_idx = -1;
+    int audio_stream_idx = -1;
+
+    const char *src_filename = NULL;
+    const char *video_dst_filename = NULL;
+    const char *audio_dst_filename = NULL;
+
+    FILE *video_dst_file = NULL;
+    FILE *audio_dst_file = NULL;
+
 
     if (argc != 4) {
         fprintf(stderr, "usage: %s  input_file video_output_file audio_output_file\n"
@@ -123,6 +125,7 @@ int main(int argc, char *argv[])
                 argv[0]);
         exit(1);
     }
+
     src_filename = argv[1];
     video_dst_filename = argv[2];
     audio_dst_filename = argv[3];
@@ -142,32 +145,84 @@ int main(int argc, char *argv[])
     /* dump input information to stderr */
     av_dump_format(fmt_ctx, 0, src_filename, 0);
 
-    if (!audio_stream && !video_stream) {
-        fprintf(stderr, "Could not find audio or video stream in the input, aborting\n");
-        ret = 1;
-        goto end;
+    //find video stream index
+    ret = av_find_best_stream(fmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);
+    if (ret < 0) {
+        fprintf(stderr, "Could not find %s stream in input file '%s'\n",
+                av_get_media_type_string(AVMEDIA_TYPE_AUDIO), src_filename);
+        return ret;
+    } else {
+        video_stream_idx = ret;
     }
+
+    //find audio stream index
+    ret = av_find_best_stream(fmt_ctx, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0);
+    if (ret < 0) {
+        fprintf(stderr, "Could not find %s stream in input file '%s'\n",
+                av_get_media_type_string(AVMEDIA_TYPE_AUDIO), src_filename);
+        return ret;
+    } else {
+        audio_stream_idx = ret;
+    }
+
 
     /* initialize packet, set data to NULL, let the demuxer fill it */
     av_init_packet(&pkt);
     pkt.data = NULL;
     pkt.size = 0;
 
-    if (video_stream)
-        printf("Demuxing video from file '%s' into '%s'\n", src_filename, video_dst_filename);
-    if (audio_stream)
-        printf("Demuxing audio from file '%s' into '%s'\n", src_filename, audio_dst_filename);
+    video_dst_file = fopen(video_dst_filename, "wb+");
+    if (!video_dst_file) {
+        printf("open %s failed!\n", video_dst_filename);
+        goto end;
+    }
+
+    audio_dst_file = fopen(audio_dst_filename, "wb+");
+    if (!audio_dst_file) {
+        printf("open %s failed!\n", audio_dst_filename);
+        goto end;
+    }
+
+    ADTSContext  m_adts_ctx;
+    AVBitStreamFilterContext *m_video_bsf;
+    AVBitStreamFilterContext *m_audio_bsf;
+    m_video_bsf = av_bitstream_filter_init("h264_mp4toannexb");
+    m_audio_bsf = av_bitstream_filter_init("aac_adtstoasc");
 
     /* read frames from the file */
     while (av_read_frame(fmt_ctx, &pkt) >= 0) {
-        // check if the packet belongs to a stream we are interested in, otherwise
-        // skip it
         if (pkt.stream_index == video_stream_idx) {
-            //todo
-            //handle video h264 data
+            uint8_t *out_data = NULL;
+            int out_size = 0;
+
+            int err = av_bitstream_filter_filter(m_video_bsf, fmt_ctx->streams[video_stream_idx]->codec, NULL, &out_data, &out_size, pkt.data, pkt.size, pkt.flags & AV_PKT_FLAG_KEY);
+            if (err <=0 ) {
+                printf("%s, run read, av_bitstream_filter_filter failed\n", TAG);
+            }
+
+
+            int ret = fwrite(out_data, 1, out_size, video_dst_file);
+//            int ret = fwrite(pkt.data, 1, pkt.size, video_dst_file);
+            if (ret != out_size) {
+                printf("fwrite video pkt failed!\n");
+            }
+
         } else if (pkt.stream_index == audio_stream_idx) {
-            //todo
-            //handle audio aac data
+            aac_decode_extradata(&m_adts_ctx, fmt_ctx->streams[audio_stream_idx]->codec->extradata, fmt_ctx->streams[audio_stream_idx]->codec->extradata_size);
+
+            unsigned char adtsHdr[ADTS_HEADER_SIZE] = {0};
+            aac_set_adts_head(&m_adts_ctx, adtsHdr, pkt.size);
+
+
+            int ret = fwrite(adtsHdr, 1, ADTS_HEADER_SIZE, audio_dst_file);
+            if (ret != ADTS_HEADER_SIZE) {
+                printf("fwrite audio adts header failed!\n");
+            }
+
+            ret = fwrite(pkt.data, 1, pkt.size, audio_dst_file);
+            if (ret != pkt.size) {
+                printf("fwrite audio pkt failed!\n");
+            }
         }
 
         av_packet_unref(&pkt);
@@ -177,42 +232,14 @@ int main(int argc, char *argv[])
 
     printf("Demuxing succeeded.\n");
 
-    if (video_stream) {
-        printf("Play the output video file with the command:\n"
-               "ffplay -f rawvideo -pix_fmt %s -video_size %dx%d %s\n",
-               av_get_pix_fmt_name(pix_fmt), width, height,
-               video_dst_filename);
-    }
-
-    if (audio_stream) {
-        enum AVSampleFormat sfmt = audio_dec_ctx->sample_fmt;
-        int n_channels = audio_dec_ctx->channels;
-        const char *fmt;
-
-        if (av_sample_fmt_is_planar(sfmt)) {
-            const char *packed = av_get_sample_fmt_name(sfmt);
-            printf("Warning: the sample format the decoder produced is planar "
-                   "(%s). This example will output the first channel only.\n",
-                   packed ? packed : "?");
-            sfmt = av_get_packed_sample_fmt(sfmt);
-            n_channels = 1;
-        }
-
-        if ((ret = get_format_from_sample_fmt(&fmt, sfmt)) < 0)
-            goto end;
-
-        printf("Play the output audio file with the command:\n"
-               "ffplay -f %s -ac %d -ar %d %s\n",
-               fmt, n_channels, audio_dec_ctx->sample_rate,
-               audio_dst_filename);
-    }
-
 end:
+    fclose(video_dst_file);
+    fclose(audio_dst_file);
+
+    av_bitstream_filter_close(m_video_bsf);
+    av_bitstream_filter_close(m_audio_bsf);
+
     avformat_close_input(&fmt_ctx);
-    av_free(video_dst_data[0]);
 
     return ret < 0;
-
-    getchar();
-    return 0;
 }
